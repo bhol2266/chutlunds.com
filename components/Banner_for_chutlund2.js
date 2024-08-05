382e456706a6de1de661f753e9dc49e6b0d10334
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-08-05 15:19:33 +0545

    sdafdsa

--- components/Banner_for_chutlund2.js
+++ components/Banner_for_chutlund2.js
@@ -1,9 +1,15 @@
-// components/Banner.js
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Flag from 'react-world-flags';
 
 export default function Banner_for_chutlund2() {
   const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const currentDomain = window.location.hostname;
+    if (currentDomain === 'chutlunds2.com') {
+      setIsVisible(false);
+    }
+  }, []);
 
   if (!isVisible) return null;
 
@@ -12,15 +18,11 @@ export default function Banner_for_chutlund2() {
       <span className='flex-1 text-left md:text-center text-sm md:text-md'>
         If this website is blocked in your country (
         <Flag code="CN" style={{ width: '20px', height: '14px' }} className="inline-block mx-1" />
-        
         <Flag code="RU" style={{ width: '20px', height: '14px' }} className="inline-block mx-1" />
-        
         <Flag code="TR" style={{ width: '20px', height: '14px' }} className="inline-block mx-1" />
-
         ) go to our alternate website   
         <a href="https://chutlunds2.com" className="underline text-yellow-500 font-inter">Chutlunds2.com</a>
-        </span>
-
+      </span>
 
       <button onClick={() => setIsVisible(false)} className="text-white">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
