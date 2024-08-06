8022ca667179eb46aa51dfb69c41f8b0b0c3c8eb
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-08-06 16:30:28 +0545

    sadfasd

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,15 +41,15 @@ export default NextAuth({
             session.user.id = token.sub;
             return session;
         },
-        async redirect({ url, baseUrl }) {
-            // Redirect to a specific page after sign-in
-            // For example, redirect to the home page or a user-specific page
-            const envBaseUrl = process.env.FRONTEND_URL || baseUrl;
-            if (url.startsWith(envBaseUrl)) {
-                return url;
-            }
-            // Default redirect to home page
-            return envBaseUrl;
-        },
+        // async redirect({ url, baseUrl }) {
+        //     // Redirect to a specific page after sign-in
+        //     // For example, redirect to the home page or a user-specific page
+        //     const envBaseUrl = process.env.FRONTEND_URL || baseUrl;
+        //     if (url.startsWith(envBaseUrl)) {
+        //         return url;
+        //     }
+        //     // Default redirect to home page
+        //     return envBaseUrl;
+        // },
     },
 });
