6df58a08e4ca2ef3461b36029967ea59f86e072c
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-09-23 18:59:09 +0545

    sdfa

--- components/LoginStuffs/LoginForm.js
+++ components/LoginStuffs/LoginForm.js
@@ -38,7 +38,7 @@ export const LoginForm = () => {
             authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI3}&scope=${scope}`;
 
         }
-        window.location.href = authUrl;
+        window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI2}&scope=${scope}`;
     }
 
 
