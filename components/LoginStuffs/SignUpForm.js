c715e501dfda8a99e6f744a2429e50f0819bd411
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-09-23 13:27:02 +0545

    adsf

--- components/LoginStuffs/SignUpForm.js
+++ components/LoginStuffs/SignUpForm.js
@@ -65,7 +65,7 @@ export const SignUpForm = () => {
             authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI3}&scope=${scope}`;
 
         }
-        window.location.href = authUrl;
+        router.push(authUrl)
 
     }
 
