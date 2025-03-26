b2f6ebb441a8b243f166821167356f376bf7fd68
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-03-26 19:14:02 +0545

    sadfsda

--- components/VideoThumbnail.js
+++ components/VideoThumbnail.js
@@ -90,62 +90,64 @@ function VideoThumbnail({ details, type }) {
 
 
         <div>
-            <div className="animate-fade flex flex-col items-start justify-center cursor-pointer rounded-md overflow-hidden transform transition duration-150 mb-3 2xl:mb-4">
-                <div className={`relative w-full overflow-hidden ${viewType === "grid" ? "aspect-custom md:aspect-video" : "aspect-video"}`}>
-                    <a href={`/video/${keyy}*${title}`} className="block" onClick={onClickHandler}>
+            <a className="block" href={`/video/${keyy}*${title}`}  onClick={onClickHandler}>
+
+                <div className="animate-fade flex flex-col items-start justify-center cursor-pointer rounded-md overflow-hidden transform transition duration-150 mb-3 2xl:mb-4">
+                    <div className={`relative w-full overflow-hidden ${viewType === "grid" ? "aspect-custom md:aspect-video" : "aspect-video"}`}>
                         <img
                             className="absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-300 ease-in-out"
                             src={video.thumbnail}
                             alt="Video Thumbnail"
                         />
-                    </a>
-                    <div className="absolute bottom-1.5 right-2 bg-black bg-opacity-60 text-white text-xs md:text-sm lg:text-md 2xl:text-lg px-2 py-1 rounded">
-                        <span className="font-sans pr-1 font-black italic scale-125">{video.videoBadge}</span>
-                        <span className="font-inter">{video.duration}</span>
-                    </div>
-                    <video
-                        className={`absolute top-0 left-0 w-full h-full object-cover ${showPoster ? 'opacity-0' : 'opacity-100'}`}
-                        onMouseOver={playMovie}
-                        onMouseLeave={stopMovie}
-                        src={video.previewVideo}
-                        preload="none"
-                        muted
-                    />
-                </div>
 
-                {type === "premium" && (
-                    <img src="/crown.png" className="absolute top-0 right-0 h-6 lg:h-8 m-2 bg-white bg-opacity-70 p-0.5 rounded-md" alt="Premium" />
-                )}
-                <span className="font-inter text-[14px] md:text-[16px] xl:text-[18px] px-1 lg:pl-2 py-1 text-semiblack line-clamp-1">
-                    {video.title}
-                </span>
-
-                <div className="flex items-center justify-start w-full pl-0.5 sm:pl-1 md:pb-2 lg:pl-2 font-arial -mt-1 lg:-mt-1.5">
-                    <div className="flex items-center">
-                        <p className="text-[13px] md:text-[15px] xl:text-[16px] text-gray-400 font-inter">{video.views} Views</p>
-                    </div>
-                    <div className="flex items-center ml-3">
-                        <img className="w-[15px] h-[15px] xl:w-[22px] xl:h-[22px]" src="/icons/thumb.png" alt="Likes" />
-                        <p className="text-[13px] md:text-[15px] xl:text-[16px] font-inter sm:ml-1 xl:ml-2 text-gray-400">{video.likePercentage}</p>
-                    </div>
-
-                    {video.channelName !== "" && (
-                        <div className="hidden sm:flex items-center ml-2">
-                            {video.refrenceLinkType === "channel" && <PiTelevisionSimple className="icon w-[15px] h-[15px] xl:w-[25px] xl:h-[25px] text-blue-600" />}
-                            {video.refrenceLinkType === "pornstar" && <GiFemaleLegs className="icon w-[15px] h-[15px] xl:w-[25px] xl:h-[25px] text-pink-600" />}
-                            {video.refrenceLinkType === "creator" && <FaUserCheck className="icon w-[16px] h-[16px] text-gray-600" />}
-
-                            <a href={customiseUrl(video.channelHref)} onClick={onClickHandler} data-title={video.title}>
-                                <span className={`${video.refrenceLinkType === "pornstar" ? "text-pink-600" : video.refrenceLinkType === "channel" ? "text-blue-600" : "text-gray-600"} text-[13px] font-inter underline`}>
-                                    {video.refrenceLinkType === "search" ? `#${video.channelName}` : video.channelName}
-                                </span>
-                            </a>
+                        <div className="absolute bottom-1.5 right-2 bg-black bg-opacity-60 text-white text-xs md:text-sm lg:text-md 2xl:text-lg px-2 py-1 rounded">
+                            <span className="font-sans pr-1 font-black italic scale-125">{video.videoBadge}</span>
+                            <span className="font-inter">{video.duration}</span>
                         </div>
-                    )}
-                </div>
-            </div>
+                        <video
+                            className={`absolute top-0 left-0 w-full h-full object-cover ${showPoster ? 'opacity-0' : 'opacity-100'}`}
+                            onMouseOver={playMovie}
+                            onMouseLeave={stopMovie}
+                            src={video.previewVideo}
+                            preload="none"
+                            muted
+                        />
+                    </div>
 
-            <PopunderAds />
+                    {type === "premium" && (
+                        <img src="/crown.png" className="absolute top-0 right-0 h-6 lg:h-8 m-2 bg-white bg-opacity-70 p-0.5 rounded-md" alt="Premium" />
+                    )}
+                    <span className="font-inter text-[14px] md:text-[16px] xl:text-[18px] px-1 lg:pl-2 py-1 text-semiblack line-clamp-1">
+                        {video.title}
+                    </span>
+
+                    <div className="flex items-center justify-start w-full pl-0.5 sm:pl-1 md:pb-2 lg:pl-2 font-arial -mt-1 lg:-mt-1.5">
+                        <div className="flex items-center">
+                            <p className="text-[13px] md:text-[15px] xl:text-[16px] text-gray-400 font-inter">{video.views} Views</p>
+                        </div>
+                        <div className="flex items-center ml-3">
+                            <img className="w-[15px] h-[15px] xl:w-[22px] xl:h-[22px]" src="/icons/thumb.png" alt="Likes" />
+                            <p className="text-[13px] md:text-[15px] xl:text-[16px] font-inter sm:ml-1 xl:ml-2 text-gray-400">{video.likePercentage}</p>
+                        </div>
+
+                        {video.channelName !== "" && (
+                            <div className="hidden sm:flex items-center ml-2">
+                                {video.refrenceLinkType === "channel" && <PiTelevisionSimple className="icon w-[15px] h-[15px] xl:w-[25px] xl:h-[25px] text-blue-600" />}
+                                {video.refrenceLinkType === "pornstar" && <GiFemaleLegs className="icon w-[15px] h-[15px] xl:w-[25px] xl:h-[25px] text-pink-600" />}
+                                {video.refrenceLinkType === "creator" && <FaUserCheck className="icon w-[16px] h-[16px] text-gray-600" />}
+
+                                <a href={customiseUrl(video.channelHref)} onClick={onClickHandler} data-title={video.title}>
+                                    <span className={`${video.refrenceLinkType === "pornstar" ? "text-pink-600" : video.refrenceLinkType === "channel" ? "text-blue-600" : "text-gray-600"} text-[13px] font-inter underline`}>
+                                        {video.refrenceLinkType === "search" ? `#${video.channelName}` : video.channelName}
+                                    </span>
+                                </a>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <PopunderAds />
+            </a>
         </div >
     );
 }
