90959e5f9d2cd6c47ffc64e7c4fc939b0233989f
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-03-31 19:22:24 +0545

    dsfsdaf

--- pages/[homepageVideos]/page/[page].js
+++ pages/[homepageVideos]/page/[page].js
@@ -99,45 +99,26 @@ export async function getStaticProps(context) {
             break;
     }
 
+    const parcelData = { url: href };
+    const API_URL = `${process.env.BACKEND_URL}getVideos`;
 
-    if (homepageVideos == "trending" && page == "1") {
+    const rawResponse = await fetch(API_URL, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(parcelData),
+    });
 
+    const { finalDataArray, pages } = await rawResponse.json();
 
-        const parcelData = { url: href };
-        const API_URL = `${process.env.BACKEND_URL}getVideos`;
-
-        const rawResponse = await fetch(API_URL, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify(parcelData),
-        });
-
-        const { finalDataArray, pages } = await rawResponse.json();
-
-        return {
-            props: {
-                video_collection: finalDataArray,
-                pages: pages
-            }
-        };
-    } else {
-
-
-        const obj = await scrapeVideos(href)
-        const finalDataArray = obj.finalDataArray
-        const pages = obj.pages
-
-        return {
-            props: {
-                video_collection: finalDataArray,
-                pages: pages
-            }
+    return {
+        props: {
+            video_collection: finalDataArray,
+            pages: pages
         }
-
-    }
+    };
 
 }
 
