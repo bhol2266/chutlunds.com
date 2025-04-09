0d3e4213f5e114a8c069fb6a4e076dfc8b396386
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-04-09 16:41:32 +0545

    sadsafd

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -105,7 +105,7 @@ export default function ContactForm({ selectedPlan }) {
                                 defaultCountry={formData.countryCode.slice(1)} // Use country code without '+'
                                 value={formData.phone}
                                 onChange={handlePhoneChange}
-                                className="w-[50%] border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 placeholder="Enter phone number"
                                 required
                             />
